e413af77c4f467773c9ffe724259d8a43c74d9f4
dongfang <lidf077@gmail.com>
2024-06-02 10:09:17 +0800

    update

--- _01_stack_heap/src/error.rs
+++ _01_stack_heap/src/error.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let name = "dong fang".to_string();
+    let name = "world".to_string();// convert &str to String
 
     std::thread::spawn(move || {
-        // function requires argument type to outlive `'static`
-        //to force the closure to take ownership of 'name' (and any other referenced variables), use the 'move' keyword
-        // may outlive borrowed value 'name'
-        println!("hello {}", name);// 'name' is borrowed here
+        println!("hello, {}", name);
     });
 }
--- _01_stack_heap/src/string.rs
+++ _01_stack_heap/src/string.rs
@@ -1,15 +1,9 @@
 fn main() {
     let data: String = "hello".into();
 
-    let s1: &str = &data;
-    let s2: &str = &data;
-    let s3: &str = &data;
+    let s1 = &data;
+    let s2 = &data;
+    let s3 = &data;
 
     dbg!(&s1 as *const _);
-    dbg!(&s2 as *const _);
-    dbg!(&s3 as *const _);
-
-    dbg!(s1.as_bytes() as *const _);
-    dbg!(s2.as_bytes() as *const _);
-    dbg!(s3.as_bytes() as *const _);
 }
