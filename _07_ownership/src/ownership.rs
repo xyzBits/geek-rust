bb5267362c9e023dc10c00b9bbf791b5269648f6
xyzBits <lidf077@gmail.com>
2024-06-21 08:49:16 +0800

    add comment

--- _07_ownership/src/ownership.rs
+++ _07_ownership/src/ownership.rs
@@ -10,6 +10,9 @@ fn main() {
     }
 }
 
+/// 动态数组因为大小在编译期间无法确定，所以放在堆上，并且在栈上有一个包含了长度和容量的胖指针指向推上的内存
+/// 每次将data作为参数传递一次，堆上的内存就会多一次引用
+/// Rust 的规则
 fn find_pos(data: Vec<u32>, v: u32) -> Option<usize> {
     for (index, item) in data.iter().enumerate() {
         if *item == v {
