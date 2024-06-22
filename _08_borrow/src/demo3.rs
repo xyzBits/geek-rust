9e17d1aed6b939f7e4c74c3c521f9afc3b329f6b
xyzBits <lidf077@gmail.com>
2024-06-22 17:45:27 +0800

    format code

--- _08_borrow/src/demo3.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut data = Vec::new();
-
-    let v = 42;
-    data.push(&v);
-
-    println!("{:?}", data);
-}
--- _08_borrow/src/demo5.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut data = vec![1, 2, 3];
-
-    for item in data.iter_mut() {
-        data.push(*item + 1);
-    }
-
-    println!("{:?}", data);
-}
