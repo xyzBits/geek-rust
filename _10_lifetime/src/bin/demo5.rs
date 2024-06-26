840a1edfdc4273a1bc65e860ff16918314e4c76a
xyzBits <lidf077@gmail.com>
2024-06-26 10:19:34 +0800

    update

--- _10_lifetime/src/bin/demo5.rs
+++ _10_lifetime/src/bin/demo5.rs
@@ -1,4 +1,12 @@
 fn main() {
+    // creates owned data from borrowed data, usually by cloning
+    let hello = "hello world".to_owned();
+
+
+    let data = &[1, 2];
+    let vector = data.to_owned();
+
+
 
 
 }
