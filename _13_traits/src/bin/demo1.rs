a2cc9ad08b1242fca2ebcef452e185a0f6faa422
xyzBits <lidf077@gmail.com>
2024-06-30 18:21:09 +0800

    impl Write trait

--- _13_traits/src/bin/demo1.rs
+++ _13_traits/src/bin/demo1.rs
@@ -31,6 +31,8 @@ impl Debug for BufBuilder {
     }
 }
 
+/// 实现了 write flush 其它的方法都使用默认的实现
+/// 这样 BufBuilder 对 Write trait 的实现是完整的，
 impl Write for BufBuilder {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         // 把 buf 添加到 BufBuilder 的尾部
@@ -45,4 +47,19 @@ impl Write for BufBuilder {
 }
 
 
-fn main() {}
+fn main() {
+    let mut buf = BufBuilder::new();
+    buf.write_all(b"hello world!").unwrap();
+    println!("{:?}", buf);
+}
+
+
+/// Clones and appends all element sin a slice to the Vec
+/// Iterates over the slice other, clones each element, and then appends it to this Vec
+/// The other slice is traversed in order
+#[test]
+fn test_extend_from_slice() {
+    let mut vec = vec![1];
+    vec.extend_from_slice(&[2, 3, 4]);
+    assert_eq!(vec, [1, 2, 3, 4]);
+}
