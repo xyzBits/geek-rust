7faf0d030b4316c133d2055f1eb96c63f252ddc4
xyzBits <lidf077@gmail.com>
2024-07-07 09:14:06 +0800

    box

--- _15_smart_pointers/src/bin/demo2.rs
+++ _15_smart_pointers/src/bin/demo2.rs
@@ -1,4 +1,4 @@
-
+use jemalloctor::Jemalloc;
 /// Box<T>
 /// Box 是 Rust 中最基本的在堆上分配内存的方式，
 /// 绝大多数其他包含堆内存的数据类型，内部都是通过 Box<T> 完成的，比如 Vec<T>
@@ -19,8 +19,21 @@
 /// 就是为了有效地利用剩余内存，并控制内存在分配和释放过程中产生的碎片数量，
 /// 在多核环境下，它还能够高效地处理并发请求，
 ///
-/// 堆上分配内存的 Box<T> 其实还有一个缺省的泛型参数 A
+/// 堆上分配内存的 Box<T> 其实还有一个缺省的泛型参数 A，
+/// 就需要满足 Allocator trait 并且默认是 Global
+/// pub struct Box<T: ?Sized, A: Allocator = Global>(Unique<T>, A)
 ///
+/// Allocator trait 提供很多方法，
+/// allocate 是主要方法，用于分配内存，对应 C 的 malloc calloc,
+/// deallocate 用于释放内存，对应 C 的 free
+/// 还有 grow shrink 用来扩大或缩小堆上已 分配的内存，对应 C 的 realloc
+///
+/// 如果你想替换默认的内存分配器，可以使用 #[global_allocator] 标记，定义你自己的全局分配器，
+/// 下面的代码展示了如何 在 Rust 下使用 jemalloc
+///
+///
+
+static GLOBAL: Jemalloc = Jemalloc;
 fn main() {
     let hello = String::from("hello");
     let len = hello.len();
