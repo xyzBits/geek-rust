d3135793b6c2055b5b8174b240a6ba330afdbec0
xyzBits <lidf077@gmail.com>
2024-07-09 09:26:11 +0800

    two sum

---
+++ _16_data_structure/src/bin/two_sum.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for (index, &value) in nums.iter().enumerate() {
+            let remain = target - value;
+            match map.get(&remain) {
+                None => {
+                    map.insert(value, index as i32);
+                }
+                Some(&remain_index) => {
+                    return vec![index as i32, remain_index];
+                }
+            }
+        }
+
+        vec![]
+    }
+}
