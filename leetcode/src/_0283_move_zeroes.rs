09767a4e907818b1fd228cfbe023ac34142111fb
xyzBits <lidf077@gmail.com>
2024-07-10 09:14:12 +0800

    update

--- leetcode/src/_0283_move_zeroes.rs
+++ leetcode/src/_0283_move_zeroes.rs
@@ -5,9 +5,11 @@ impl Solution {
         let mut offset  = 0_usize;
 
         for i in 0..nums.len() {
+            // 碰到 为 0 的元素，就将 offset ++
             if nums[i] == 0 {
                 offset += 1;
             } else if offset != 0 {
+                // offset 不为 0 时，进行替换，并将 i 位置上的元素置为 0
                 nums[i - offset] = nums[i];
                 nums[i] = 0;
             }
