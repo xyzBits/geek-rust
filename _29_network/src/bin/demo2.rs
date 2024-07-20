f5dcf5e0bdefa4ee233add5ae5a1ea0f8c1174eb
xyzBits <lidf077@gmail.com>
2024-07-20 18:41:12 +0800

    network

--- _29_network/src/bin/demo2.rs
+++ _29_network/src/bin/demo2.rs
@@ -4,6 +4,10 @@ use std::net::TcpStream;
 /// 对于客户端，可以使用 Tcp::connect() 得到一个 TcpStream，一旦客户端的请求被服务器接受
 /// 就可以发送或者接收数据
 ///
+/// 从客户端代码可以看出，我们无需显式的关闭 TcpStream，因为 TcpStream 内部也实现了 Drop trait
+/// 使得其离开作用域时会被关闭
+///
+///
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:9527").unwrap();
